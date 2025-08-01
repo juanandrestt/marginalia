823125b81431ad229b44e1b5f95331a4851244df
Juan Andres Trujillo <juan.trujillo1@protonmail.com>
2025-08-01 16:47:10 +0200

    import local font aujournuit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+const Aujournuit = localFont({
+	src: "./fonts/Aujournuit-Regular.woff2",
+	variable: "--font-aujournuit",
 });
 
 export const metadata: Metadata = {
@@ -24,10 +19,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-				{children}
-			</body>
+			<body className={`${Aujournuit.variable} antialiased`}>{children}</body>
 		</html>
 	);
 }
