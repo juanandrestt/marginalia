1c8b7daedcef91f51790c3449bd1b4dbed333248
Juan Andres Trujillo <juan.trujillo1@protonmail.com>
2025-08-12 14:58:56 +0200

    add links

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
 	return (
 		<>
-			<header>
-				<a href='https://www.juantrujillo.com'>by Juan Trujillo</a>
-				<a href=''></a>
-			</header>
+			<footer className='absolute bottom-4 right-8 flex flex-col'>
+				<a href='https://github.com/juanandrestt/marginalia'>github</a>
+				<a href='https://www.juantrujillo.world'>by Juan Trujillo</a>
+			</footer>
 
-			<main>
+			<main className='flex h-screen items-center justify-center'>
 				<span>Coming soon</span>
 			</main>
 		</>
